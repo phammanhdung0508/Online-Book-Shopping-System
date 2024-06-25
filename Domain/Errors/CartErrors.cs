445c23810cd46fbb8a8c92a77c57e89229d1fbf2
Manh Dung (Sunf) <kibichan52@gmail.com>
2024-06-26 02:51:31 +0700

    Finish Handle Add And Remove Item From Cart.

--- Domain/Errors/CartErrors.cs
+++ Domain/Errors/CartErrors.cs
@@ -3,5 +3,6 @@
 public static class CartErrors
 {
     public static readonly Error NotFound = new("Cart.NotFound", "Can't not found any cart.");
-    public static readonly Error ItemExist = new("Cart.ItemExist", "This book already exist in cart.");
+    public static readonly Error ItemNotFound = new("Cart.ItemNotFound", "Book is not found in cart.");
+    public static readonly Error ItemExist = new("Cart.ItemExist", "Book already exist in cart.");
 }
